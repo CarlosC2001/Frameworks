8425bd73f8dd4e3a9c5d521bf524572cb3a10e6a
CarlosC2001 <carlos.campo@est.iudigital.edu.co>
2023-10-08 19:40:05 -0500

    Cambio

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+
+<div class="container-fluid">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-10 col-lg-10">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+                <button type="button" class="btn btn-primary btn-lg" onclick="location.href='{{ route('post') }}'">POST</button>
+                <button type="button" class="btn btn-secondary btn-lg" onclick="location.href='{{ route('categories.index') }}'">CATEGORY</button>
+ 
             </div>
         </div>
-    </div>
+        </div>
 </div>
 @endsection
